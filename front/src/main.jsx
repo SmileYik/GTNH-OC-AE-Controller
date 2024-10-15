1327dce0b47a61ff96d06a5918c32f928c973b88
SmileYik <miskyle@outlook.com>
2024-10-15 15:46:50 +0800

    Clean branch

--- front/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import MainFrame from "./MainFrame.jsx";
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <MainFrame />
-  </StrictMode>,
-)
