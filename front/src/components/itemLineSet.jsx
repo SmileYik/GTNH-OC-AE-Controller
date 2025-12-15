2590f535c257157afc76c7b5da966da267db0a66
SmileYik <miskyle@outlook.com>
2025-12-15 15:15:28 +0800

    fix: fix item line set component

--- front/src/components/itemLineSet.jsx
+++ front/src/components/itemLineSet.jsx
@@ -1,6 +1,6 @@
 const inputLineNumber = (defaultLineCount) => {
     const numberString = prompt("请输入要显示的物品行数: ", defaultLineCount + "");
-    const number = parseInt(numberString)
+	let number = parseInt(numberString)
     if (number + "" !== numberString || number === 0 || isNaN(number)) number = defaultLineCount;
     return number
 }
